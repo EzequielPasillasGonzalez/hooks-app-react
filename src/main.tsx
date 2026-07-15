013fa8e0565a420e2bd11d7517cdad547e804fe8
Ezequiel <ingepasillasg@gmail.com>
2026-07-14 22:44:33 -0600

    feat: uso de hools compartidos

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { PokemonPage } from "./03-examples/PokemonPage.tsx";
 // import { TrafficLight } from "./01-useState/TrafficLight.tsx";
-import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook.tsx";
+// import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
-    <TrafficLightWithHook />
+    {/* <TrafficLightWithHook /> */}
+    <PokemonPage />
   </StrictMode>,
 );
