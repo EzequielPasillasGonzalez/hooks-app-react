4154091b929eeba70a73801a63f322d48e1e3797
Ezequiel <ingepasillasg@gmail.com>
2026-07-15 21:48:55 -0600

    feat: instalacion de shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { TasksApp } from "./05-useReducer/TaskApp.tsx";
 // import { PokemonPage } from "./03-examples/PokemonPage.tsx";
-import { FocusScreen } from "./04-useRef/FocusScreen.tsx";
+// import { FocusScreen } from "./04-useRef/FocusScreen.tsx";
 // import { TrafficLight } from "./01-useState/TrafficLight.tsx";
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook.tsx";
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 );
